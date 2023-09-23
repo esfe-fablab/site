cef27359a439b0595db9b9e64ce047c8a4f5afa8
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-23 15:12:06 -0600

    chore: :sparkles: add services and DTO/

--- FABLAB.Site.BLL/Services/Utils/EmailService.cs
+++ FABLAB.Site.BLL/Services/Utils/EmailService.cs
@@ -1,53 +1,44 @@
-﻿using FABLAB.Site.DAL.DTOs;
+﻿using FABLAB.Site.BLL.DTOs;
 using MailKit.Security;
-using Microsoft.Extensions.Configuration;
 using MimeKit.Text;
 using MimeKit;
 using MailKit.Net.Smtp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace FABLAB.Site.BLL.Services.Utils;
-public class EmailService : IEmailService
+namespace FABLAB.Site.BLL.Services.Utils
 {
-    private readonly IConfiguration _config;
-    public EmailService(IConfiguration config)
+    public class EmailService : IEmailService
     {
-        _config = config;
-    }
-
-    public bool SendEmail(EmailDTO emailDTO)
-    {
-        try
+        
+        public bool SendEmail(EmailDTO emailDTO)
         {
-            var Host = _config.GetSection("Email:Host").Value;
-            int Port = Convert.ToInt32(_config.GetSection("Email:Port").Value);
-            var UserName = _config.GetSection("Email:UserName").Value;
-            var Password = _config.GetSection("Email:Password").Value;
-
-            var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse(UserName));
-            email.To.Add(MailboxAddress.Parse(emailDTO.To));
-            email.Subject = emailDTO.Subject;
-            email.Body = new TextPart(TextFormat.Html)
+            try
             {
-                Text = emailDTO.Content
-            };
+                var Host = "";
+                int Port = 0;
+                var UserName = "";
+                var Password = "";
 
-            using var smtp = new SmtpClient();
-            smtp.Connect(Host, Port, SecureSocketOptions.StartTls);
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse(UserName));
+                email.To.Add(MailboxAddress.Parse(emailDTO.To));
+                email.Subject = emailDTO.Subject;
+                email.Body = new TextPart(TextFormat.Html)
+                {
+                    Text = emailDTO.Content
+                };
 
-            smtp.Authenticate(UserName, Password);
-            smtp.Send(email);
-            smtp.Disconnect(true);
-            return true;
-        }
-        catch
-        {
-            return false;
+                using var smtp = new SmtpClient();
+                smtp.Connect(Host, Port, SecureSocketOptions.StartTls);
+
+                smtp.Authenticate(UserName, Password);
+                smtp.Send(email);
+                smtp.Disconnect(true);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
---
+++ FABLAB.Site.BLL/Services/Utils/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using FABLAB.Site.BLL.DTOs;
+
+namespace FABLAB.Site.BLL.Services.Utils;
+
+public interface IEmailService
+{
+  bool SendEmail(EmailDTO emailDTO);
+}
