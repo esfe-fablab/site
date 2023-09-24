7dd57486dd8d30df72216a70ce631f2cc367c642
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-24 14:07:53 -0600

    chore: :sparkles: Article Service

---
+++ FABLAB.Site.BLL/Services/ArticleService.cs
@@ -0,0 +1,33 @@
+﻿using FABLAB.Site.DAL;
+using FABLAB.Site.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FABLAB.Site.BLL.Services
+{
+    public class ArticleService
+    {
+        private readonly IGenericRepository<Article> repo;
+        public ArticleService(IGenericRepository<Article> _repo)
+        {
+            repo = _repo;
+        }
+
+        public async Task<bool> Create(Article model)
+        {
+            return await repo.Create(model);
+        }
+
+        public Task<bool> Delete(int id) => repo.Delete(id);
+
+        public Task<IQueryable<Article>> GetAll() => repo.GetAll();
+
+        public async Task<Article?> GetById(int id) => await repo.GetById(id);
+
+        public Task<bool> Update(Article model) => repo.Update(model);
+
+    }
+}
---
+++ FABLAB.Site.BLL/Services/IArticleService.cs
@@ -0,0 +1,18 @@
+﻿using FABLAB.Site.EN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FABLAB.Site.BLL.Services
+{
+    public interface IArticleService
+    {
+        Task<bool> Create(Article model);
+        Task<bool> Update(Article model);
+        Task<bool> Delete(int id);
+        Task<IQueryable<Article>> GetAll();
+        Task<Article?> GetById(int id);
+    }
+}
