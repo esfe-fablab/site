a499377121aace2b09ba94ead07b90ca825cf5db
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-24 06:57:28 -0600

    chore: :sparkles: dependencie inyection and conf jwt

--- FABLAB.Site.DAL/DataContext/FabLabDbContext.cs
+++ FABLAB.Site.DAL/DataContext/FabLabDbContext.cs
@@ -34,10 +34,7 @@ public partial class FabLabDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;DataBase=FabLabDB;Integrated Security=true;Encrypt=false");
-
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Article>(entity =>
