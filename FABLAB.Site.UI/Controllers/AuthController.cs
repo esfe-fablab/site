b93b59ad43aa80114aa459fa77cc8f766d387641
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-24 06:58:53 -0600

    chore: :sparkles: Implements Auth and Test

---
+++ FABLAB.Site.UI/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using FABLAB.Site.UI.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FABLAB.Site.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+        [HttpPost]
+        [Route("getAuth")]
+        public async Task<IActionResult> Autenticar([FromBody] AuthRequest autorizacion)
+        {
+            var resultado_autorizacion = await _authService.GetToken(autorizacion);
+            if (resultado_autorizacion == null)
+                return Unauthorized();
+
+            return Ok(resultado_autorizacion);
+
+        }
+    }
+}
--- FABLAB.Site.UI/Program.cs
+++ FABLAB.Site.UI/Program.cs
@@ -1,8 +1,52 @@
+using FABLAB.Site.BLL.Services;
+using FABLAB.Site.DAL;
+using FABLAB.Site.DAL.DataContext;
+using FABLAB.Site.DAL.Repositories;
+using FABLAB.Site.EN;
+using FABLAB.Site.UI.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<FabLabDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FabLabDBContext"));
+});
+
+
+builder.Services.AddScoped<IGenericRepository<SystemUser>, SystemUserRepository>();
+builder.Services.AddScoped<ISystemUserService, SystemUserService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+var key = builder.Configuration.GetValue<string>("JwtSettings:key");
+var keyBytes = Encoding.ASCII.GetBytes(key);
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
 
 var app = builder.Build();
 
@@ -13,10 +57,14 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
