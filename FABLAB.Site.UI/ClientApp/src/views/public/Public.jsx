01e07ecb70f4757662b032e8bc04d46be88bb6d8
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-09-27 17:47:11 -0600

    chore: sparkles: route ViewBlog

--- FABLAB.Site.UI/ClientApp/src/views/public/Public.jsx
+++ FABLAB.Site.UI/ClientApp/src/views/public/Public.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
-import { Inicio, Blog, Aprende} from './pages'
+import { Inicio, Blog, BlogView, Aprende} from './pages'
 
 import '../../style/public/index.css'
 
@@ -11,6 +11,7 @@ const Public = () => {
       <Routes>
         <Route path='/' element={<Inicio />}></Route>
         <Route path='/blog' element={<Blog />}></Route>
+        <Route path='/blog/:idBlog' element={<BlogView />}></Route>
         <Route path='/aprende' element={<Aprende />}></Route>
       </Routes>
     </Layout>
